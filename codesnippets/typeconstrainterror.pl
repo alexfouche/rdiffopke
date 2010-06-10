bfc7e746117c94200028e14a9949be22d31b8bbc
Alexandre Fouche <alexandre.fouche@gmail.com>
2010-06-10 18:11:56 +0800

    ras

---
+++ codesnippets/typeconstrainterror.pl
@@ -0,0 +1,34 @@
+package Something::File;
+use Moose;
+has 'type' =>(is=>'ro', isa=>'Str', writer=>'_set_type' );
+
+sub is_slink {
+	my $self = shift;
+	return ( $self->type eq 'slink' );
+}
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+1;
+
+
+package Something::File::LocalFile;
+use Moose;
+use Moose::Util::TypeConstraints;
+
+extends 'Something::File';
+
+subtype 'PositiveInt'
+ 	 =>	as 'Int'
+     => where { $_ >0 }
+     => message { 'Only positive greater than zero integers accepted' };
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+1;
+
+
+my $a = Something::File::LocalFile->new;
+#$a->_set_type('slink');
+print $a->is_slink ." end\n"; 
+
